b43dd2790973390350e609a108c7af3775c551e7
Ben Burge <benburgecode@gmail.com>
2023-10-26 11:45:29 -0600

    Uploading Net8 Blazor Template using MudBlazor

---
+++ Components/Layout/LayoutState.cs
@@ -0,0 +1,25 @@
+﻿namespace NET8SERVER.Components.Layout
+{
+    public class LayoutState
+    {
+        public event Action? OnChange;
+
+        public bool IsDrawerOpen { get; private set; }
+
+        public bool IsDarkMode { get; private set; }
+
+        public void ToggleDrawer()
+        {
+            IsDrawerOpen = !IsDrawerOpen;
+            NotifyStateChanged();
+        }
+
+        public void ToggleDarkMode()
+        {
+            IsDarkMode = !IsDarkMode;
+            NotifyStateChanged();
+        }
+
+        public void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
